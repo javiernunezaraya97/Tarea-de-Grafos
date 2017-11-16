86fb406d3f06b27fd6b3cc34297d12541f992e1d
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2017-11-16 07:28:45 -0600

    Avance Cola Prioridad

--- TareaGrafos/ColaDePrioridad.h
+++ TareaGrafos/ColaDePrioridad.h
@@ -13,12 +13,74 @@
 
 #ifndef COLADEPRIORIDAD_H
 #define COLADEPRIORIDAD_H
+#define SIZE 10
+#include <iostream>
+#include <string>
+#include <memory>
+#include <sstream>
+using namespace std;
 
+template <class T>
 class ColaDePrioridad {
 public:
+    T heap [SIZE];
+    int ultimo;
+    int contador;
+    /*
+  EFE: Crea una Cola vacia 
+  REQ: Cola no inicializada
+  MOD:
+*/
     ColaDePrioridad();
-    ColaDePrioridad(const ColaDePrioridad& orig);
+//    ColaDePrioridad(const ColaDePrioridad& orig);
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
     virtual ~ColaDePrioridad();
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void vaciar();
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    bool vacia();
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void agregar(T elemento,int prioridad);
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    T sacar();
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void modificarPrioridad(T elemento, int nuevaPrioridad);
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    int numElem();
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void borrar(T elemento);
 private:
 
 };
