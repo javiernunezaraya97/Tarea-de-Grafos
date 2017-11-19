b0174ccf24a1589ac897e8b7d9af65e9ca4519d1
roonike <fitzberth.powellchanner@ucr.ac.cr>
2017-11-18 19:34:22 -0600

    Trabajo en Conjunto de Conjuntos

--- TareaGrafos/ConjuntoDeConj.h
+++ TareaGrafos/ConjuntoDeConj.h
@@ -6,21 +6,86 @@
 
 /* 
  * File:   ConjuntoDeConj.h
- * Author: Javier
+ * Author: Fitzberth
  *
  * Created on 5 de noviembre de 2017, 11:30 p.m.
  */
 
 #ifndef CONJUNTODECONJ_H
 #define CONJUNTODECONJ_H
-
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
+template <class T>
 class ConjuntoDeConj {
 public:
+    //EFE:Constructuye el conjunto de conjunto disjutnos
+    //REQ:---
+    //MOD:
     ConjuntoDeConj();
-    ConjuntoDeConj(const ConjuntoDeConj& orig);
+    //EFE:
+    //REQ:
+    //MOD:
     virtual ~ConjuntoDeConj();
+    //EFE:
+    //REQ:
+    //MOD:
+    void vaciar();
+    //EFE:
+    //REQ:
+    //MOD:
+    bool vacio();
+    //EFE:
+    //REQ:
+    //MOD:
+    string conjuntoAlQuePertenece(T elemento);
+    //EFE:
+    //REQ:
+    //MOD:
+    void agregarConjunto(string Identificador, T elemento);
+    //EFE:
+    //REQ:
+    //MOD:
+    void unirConjuntos(T conj1, T conj2);
 private:
-
+    template <typename C>
+    struct ConjuntoDeConjuntos{
+        struct Conjunto{
+        C elemento;    
+        Conjunto *sig;
+        Conjunto(C e){
+                Conjunto.elemento = e;
+                Conjunto.sig = nullptr;
+            };
+        }
+        string identificador;
+        ConjuntoDeConjuntos *sig;
+        Conjunto *conjuntos;
+        ConjuntoDeConjuntos(string id, C e){
+            identificador = id;
+            conjuntos = new Conjunto(elemento);
+            sig = nullptr;
+        }
+    };
+    ConjuntoDeConjuntos *primero;
+    template <typename T>
+    ConjuntoDeConj<T>::ConjuntoDeConj(){
+        
+    }
+    ConjuntoDeConj<T>::~ConjuntoDeConj(){
+        
+    }
+    void ConjuntoDeConj<T>::agregarConjunto(string Identificador, T elemento){
+        
+    }
+    string ConjuntoDeConj<T>::conjuntoAlQuePertenece(){
+        
+    }
+    void ConjuntoDeConj<T>::unirConjuntos(T conj1, T conj2){
+        
+    }
 };
 
 #endif /* CONJUNTODECONJ_H */
