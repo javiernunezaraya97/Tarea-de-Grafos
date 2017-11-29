2423f974e602cc806ea14056efc8d1cb7ba7aa1b
Luis Carmona Castro <b51558@ecci.ucr.ac.cr>
2017-11-29 15:21:13 -0600

    conj de conj terminado

--- TareaGrafos/main.cpp
+++ TareaGrafos/main.cpp
@@ -15,6 +15,7 @@
 //#define SIZE 10
 #include "ListaDeAdy.h"
 #include "ColaDePrioridad.h"
+#include "ConjuntoDeConj.h"
 #include <string>
 using namespace std;
 
@@ -22,16 +23,41 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    ColaDePrioridad<string>* cola = new ColaDePrioridad<string>;
-
-    cola->agregar("a", 1);
-    cola->agregar("s", 2);
-
-    cola->agregar("Sandra", 7);
-    int num = 2/*cola->numElem()*/;
-    for (int i = 0; i < num; i++) {
-        cout << "Elemento: " << i << cola->sacar() << "\n";
-    }
+    
+    ConjuntoDeConj<int>* prueba= new ConjuntoDeConj<int>;
+    
+    prueba->agregarConjunto("c", 5);
+    prueba->agregarConjunto("d", 6);
+    prueba->agregarConjunto("e", 7);
+    prueba->agregarConjunto("k", 17);
+    prueba->agregarConjunto("j", 71);
+    prueba->agregarConjunto("i", 73);
+    prueba->agregarConjunto("h", 27);
+    prueba->agregarConjunto("g", 24);
+    prueba->agregarConjunto("f", 74);
+    prueba->agregarConjunto("b", 75);
+    prueba->agregarConjunto("a", 78);
+    
+    string pert= prueba->conjuntoAlQuePertenece(6);
+    prueba->unirConjuntos("c", "e");
+    prueba->unirConjuntos("d", "c");
+    prueba->unirConjuntos("d", "k");
+    prueba->unirConjuntos("d", "f");
+    prueba->unirConjuntos("d", "b");
+    prueba->unirConjuntos("d", "a");
+    
+    
+    int jj=0;
+//    ColaDePrioridad<string>* cola = new ColaDePrioridad<string>;
+//
+//    cola->agregar("a", 1);
+//    cola->agregar("s", 2);
+//
+//    cola->agregar("Sandra", 7);
+//    int num = 2/*cola->numElem()*/;
+//    for (int i = 0; i < num; i++) {
+//        cout << "Elemento: " << i << cola->sacar() << "\n";
+//    }
 
     grafo *g = new grafo;
 
