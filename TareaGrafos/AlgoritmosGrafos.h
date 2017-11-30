35b70aa721c8d71b57807e184ab681d5652913e6
Luis Carmona Castro <b51558@ecci.ucr.ac.cr>
2017-11-30 17:44:25 -0600

    funk

--- TareaGrafos/AlgoritmosGrafos.h
+++ TareaGrafos/AlgoritmosGrafos.h
@@ -13,14 +13,56 @@
 
 #ifndef ALGORITMOSGRAFOS_H
 #define ALGORITMOSGRAFOS_H
-
+#include <utility> 
+#include "ColaDePrioridad.h"
+#include "ConjuntoDeConj.h"
+#include "Diccionario.h"
+#include "ListaDeAdy.h"
+//#include "MatrizDeAdy.h"
+using namespace std; 
 class AlgoritmosGrafos {
 public:
     AlgoritmosGrafos();
     AlgoritmosGrafos(const AlgoritmosGrafos& orig);
     virtual ~AlgoritmosGrafos();
 private:
-
+/*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void Dijkstra;
+    
+/*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void Floyd;
+/*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void Prim;
+/*
+  EFE:
+  REQ:
+  MOD:
+*/
+    void Kruskal;
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    grafo Copiar;
+    /*
+  EFE:
+  REQ:
+  MOD:
+*/
+    bool Iguales;
 };
 
 #endif /* ALGORITMOSGRAFOS_H */
