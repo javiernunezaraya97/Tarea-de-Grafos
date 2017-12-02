9116b6aac2b4ce67ca57d6ecba627d7356c963dd
JAVIER-PC\Javier <javier.nunezaraya@ucr.ac.cr>
2017-12-02 11:11:23 -0600

    main pruebas

--- TareaGrafos/main.cpp
+++ TareaGrafos/main.cpp
@@ -20,157 +20,46 @@
 #include "ColaDePrioridad.h"
 #include "ConjuntoDeConj.h"
 #include <string>
+#include <ctime>
 using namespace std;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
-    string p;
+    srand(10041997);
+    
     grafo *g = new grafo;
     AlgoritmosGrafos AG;
     vertice vj;
-    for (int i = 0; i <= 50; ++i) {
-        vj = g->agregarVertice(to_string(i));
-        if (i > 0 && i < 50) {
-            g->agregarArista(g->primerVertice(), g->sigVertice(vj), 3);
+//    for (int i = 0; i <= 50; ++i) {
+//        vj = g->agregarVertice(to_string(i));
+//        if (i > 0 && i < 50) {
+//            g->agregarArista(g->primerVertice(), g->sigVertice(vj), 3);
+//        }
+//    }
+    vertice v1;
+    vertice v2;
+    int n=20;
+    int p;
+    for (int i = 0; i < n; ++i){
+        g->agregarVertice(to_string(i));
+    }
+    for (int i = 0; i < n; ++i){
+        
+        v1 = AG.buscarEtiq(to_string(i),*g);
+        for (int j = 0; j < n/3; ++j)
+        {
+            p = (rand()%20) + 1;
+            v2 = AG.buscarEtiq(to_string(rand()%n),*g);
+            while ((g->adyacentes(v1, v2)) || (v1 == v2))
+            {
+                v2 = AG.buscarEtiq(to_string(rand()%n),*g);
+            }
+            g->agregarArista(v1,v2,p);
         }
     }
-    //    AG.Kruskal(*g);
-    //       AG.Dijkstra(g->primerVertice(),*g);
-    //       grafo copia = AG.Copiar(*g);
-
-    Diccionario<string>* dicc = new Diccionario<string>;
-    dicc->agregar("Hola");
-    dicc->agregar("Soy");
-    dicc->agregar("Una");
-    dicc->agregar("Oveja");
-    dicc->agregar("Ovejas");
-    dicc->agregar("Muchas");
-    dicc->agregar("Sence");
-    dicc->agregar("No");
-    dicc->agregar("Make");
-    cout << dicc->listar();
-
-    ColaDePrioridad<string> cp;
-    cp.iniciar();
-    cp.agregar("hola", 1);
-
-    ColaDePrioridad<string>* cola = new ColaDePrioridad<string>;
-
-
-
-
-    cola->agregar("j", 9);
-    cola->agregar("g", 8);
-    cola->agregar("a", 1);
-    cola->agregar("s", 5);
-    cola->agregar("t", 2);
-    cola->agregar("i", 3);
-    cola->agregar("y", 4);
-    cola->agregar("r", 6);
-    cola->agregar("f", 7);
-    cola->agregar("e", 10);
-
-
-
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    p = cola->sacar();
-    cout << "Elemento: " /*<< i+1*/ << cola->sacar() << "\n";
-    //            cout << "Elemento: " /*<< i+1*/ << cola->sacar() << "\n";
-    //            cout << "Elemento: " /*<< i+1*/ << cola->sacar() << "\n";
-    //            cout << "Elemento: " /*<< i+1*/ << cola->sacar() << "\n";
-    //    int num = cola->numElem();
-    //    for (int i = 0; i < num; i++) {
-    //        cout << "Elemento: " << i + 1 << cola->sacar() << "\n";
-    //    }
-
-    ConjuntoDeConj<int>* prueba = new ConjuntoDeConj<int>;
-
-    prueba->agregarConjunto("c", 5);
-    prueba->agregarConjunto("d", 6);
-    prueba->agregarConjunto("e", 7);
-    prueba->agregarConjunto("k", 17);
-    prueba->agregarConjunto("j", 71);
-    prueba->agregarConjunto("i", 73);
-    prueba->agregarConjunto("h", 27);
-    prueba->agregarConjunto("g", 24);
-    prueba->agregarConjunto("f", 74);
-    prueba->agregarConjunto("b", 75);
-    prueba->agregarConjunto("a", 78);
-
-    string pert = prueba->conjuntoAlQuePertenece(6);
-    prueba->unirConjuntos("c", "e");
-    prueba->unirConjuntos("d", "c");
-    prueba->unirConjuntos("d", "k");
-    prueba->unirConjuntos("d", "f");
-    prueba->unirConjuntos("d", "b");
-    prueba->unirConjuntos("d", "a");
-
-
-    int jj = 0;
-    //    ColaDePrioridad<string>* cola = new ColaDePrioridad<string>;
-    //
-    //    cola->agregar("a", 1);
-    //    cola->agregar("s", 2);
-    //
-    //    cola->agregar("Sandra", 7);
-    //    int num = 2/*cola->numElem()*/;
-    //    for (int i = 0; i < num; i++) {
-    //        cout << "Elemento: " << i << cola->sacar() << "\n";
-    //    }
-
-
-
-
-    vertice v1 = g->agregarVertice("abc");
-    cout << "etiqueta: " << v1->etiqueta << "\n";
-
-    vertice v2 = g->agregarVertice("katherine fea");
-    cout << "etiqueta2: " << v2->etiqueta << "\n";
-    g->agregarArista(v1, v2, 10);
-    bool esAdy = g->adyacentes(v1, v2);
-    cout << "peso: " << g->Peso(v1, v2) << "\n";
-    v2 = g->agregarVertice("k fea");
-    g->agregarArista(v1, v2, 14);
-    esAdy = g->adyacentes(v1, v2);
-    cout << v2->etiqueta << "\n" << esAdy << "\n";
-    cout << "peso: " << g->Peso(v1, v2) << "\n";
-    v2 = g->agregarVertice("ka fea");
-    g->agregarArista(v1, v2, 6);
-    esAdy = g->adyacentes(v1, v2);
-    cout << "peso: " << g->Peso(v1, v2) << "\n";
-    g->modificarPeso(v1, v2, 22);
-
-    cout << "peso nuevo: " << g->Peso(v1, v2) << "\n";
-    cout << "peso nuevo: " << g->Peso(v2, v1) << "\n";
-    cout << v1->etiqueta << "\n" << esAdy << "\n";
-    cout << v2->etiqueta << "\n";
-    cout << g->numVertices() << "\n";
-    cout << g->numVerticesAdy(v1) << "\n";
-
-
-    v2 = g->primerVerticeAdy(v1);
-    cout << g->Etiqueta(v2) << "\n";
-    v2 = g->sigVerticeAdy(v1, v2);
-    cout << g->Etiqueta(v2) << "\n";
-    v2 = g->sigVerticeAdy(v1, v2);
-    cout << g->Etiqueta(v2) << "\n";
-
-
-    g->vaciar();
-    bool vacio = g->vacio();
-
-
+    int j=n;
 
 
 
