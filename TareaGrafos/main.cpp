8a1e5d8f15fb7853915e3ac629034c64b221d498
JAVIER-PC\Javier <javier.nunezaraya@ucr.ac.cr>
2017-12-02 18:57:01 -0600

    main

--- TareaGrafos/main.cpp
+++ TareaGrafos/main.cpp
@@ -30,158 +30,158 @@ using namespace std;
 int main(int argc, char** argv) {
     srand(10042017);
     
-    grafo* g= new grafo;
-    AlgoritmosGrafos AG;
-    vertice vj;
-//    for (int i = 0; i <= 50; ++i) {
-//        vj = g->agregarVertice(to_string(i));
-//        if (i > 0 && i < 50) {
-//            g->agregarArista(g->primerVertice(), g->sigVertice(vj), 3);
+//    grafo* g= new grafo;
+//    AlgoritmosGrafos AG;
+//    vertice vj;
+////    for (int i = 0; i <= 50; ++i) {
+////        vj = g->agregarVertice(to_string(i));
+////        if (i > 0 && i < 50) {
+////            g->agregarArista(g->primerVertice(), g->sigVertice(vj), 3);
+////        }
+////    }
+//    vertice v1;
+//    vertice v2;
+//    int n=6;
+//    int p;
+//    for (int i = 0; i < n; ++i){
+//        g->agregarVertice(to_string(i));
+//    }
+//
+//    for (int i = 0; i < n; ++i){
+//         
+//        v1 = AG.buscarEtiq(to_string(i),*g);
+//        for (int j = 0; j < n/3; ++j)
+//        {  
+//            p = (rand()%20) + 1;
+//            v2 = AG.buscarEtiq(to_string(rand()%(n-1)), *g);
+//            while ((g->adyacentes(v1, v2)) || (v1 == v2))
+//            {   
+//                v2 = AG.buscarEtiq(to_string(rand()%n), *g);
+//            }
+//            g->agregarArista(v1,v2, p);
 //        }
 //    }
-    vertice v1;
-    vertice v2;
-    int n=6;
-    int p;
-    for (int i = 0; i < n; ++i){
-        g->agregarVertice(to_string(i));
-    }
-
-    for (int i = 0; i < n; ++i){
-         
-        v1 = AG.buscarEtiq(to_string(i),*g);
-        for (int j = 0; j < n/3; ++j)
-        {  
-            p = (rand()%20) + 1;
-            v2 = AG.buscarEtiq(to_string(rand()%(n-1)), *g);
-            while ((g->adyacentes(v1, v2)) || (v1 == v2))
-            {   
-                v2 = AG.buscarEtiq(to_string(rand()%n), *g);
-            }
-            g->agregarArista(v1,v2, p);
-        }
-    }
-    
-    grafo gCopia= AG.Copiar(*g);
-    bool igual= AG.Iguales(*g, gCopia);
-    
-    AG.Kruskal(gCopia);
-    
-    AG.Prim(gCopia);
-    
-    AG.Floyd(gCopia);
+//    
+//    grafo gCopia= AG.Copiar(*g);
+//    bool igual= AG.Iguales(*g, gCopia);
+//    
+//    AG.Kruskal(gCopia);
+//    
+//    AG.Prim(gCopia);
+//    
+//    AG.Floyd(*g);
    // AG.vendedor(gCopia);
    // AG.Dijkstra(gCopia.primerVertice(), gCopia);
 
-//    grafo* g = new grafo;
-//    vertice v;
-//    string etiqueta;
-//    AlgoritmosGrafos* algoritmos = new AlgoritmosGrafos();
-//    int caso = 0;
-//    cout << "Saludos, ¿que desea utilizar?" << "\n";
-//    cout << "Presione la tecla 1 para operadores basicos." << "\n";
-//    cout << "Presione la tecla 2 para algoritmos." << "\n";
-//    cout << "Presione la tecla 3 para salir." << "\n";
-//    bool salir = false;
-//    while (!salir) {
-//        cin >> caso;
-//        int segundoCaso = 0;
-//        switch (caso) {
-//            case 1:
-//                cout << "Presione 1 para vaciar()" << "\n";
-//                cout << "Presione 2 para vacio()" << "\n";
-//                cout << "Presione 3 para agregarVertice()" << "\n";
-//                cout << "Presione 4 para eliminarVertice()" << "\n";
-//                cout << "Presione 5 para modifiarEtiqueta()" << "\n";
-//                cout << "Presione 6 para Etiqueta()" << "\n";
-//                cout << "Presione 7 para agregarArista()" << "\n";
-//                cout << "Presione 8 para eliminarArista()" << "\n";
-//                cout << "Presione 9 para modificarPeso()" << "\n";
-//                cout << "Presione 10 para peso()" << "\n";
-//                cout << "Presione 11 para adyacentes()" << "\n";
-//                cout << "Presione 12 para primerVertice()" << "\n";
-//                cout << "Presione 13 para siguienteVertice()" << "\n";
-//                cout << "Presione 14 para primerVerticeAdy()" << "\n";
-//                cout << "Presione 15 para sigVerticeAdy()" << "\n";
-//                cout << "Presione 16 para numVertices()" << "\n";
-//                cout << "Presione 17 para numVerticesAdy()" << "\n";
-//                cin >> segundoCaso;
-//                break;
-//
-//            case 2:
-//                cout << "Presione 1 para Dijkstra()" << "\n";
-//                cout << "Presione 2 para Floyd()" << "\n";
-//                cout << "Presione 3 para Prim()" << "\n";
-//                cout << "Presione 4 para Kruskal()" << "\n";
-//                cout << "Presione 5 para Copiar()" << "\n";
-//                cout << "Presione 6 para Iguales()" << "\n";
-//                cout << "Presione 7 para Vendedor()"<< "\n";
-//                cin >> segundoCaso;
-//                break;
-//            case 3:
-//                cout << "Adios" << "\n";
-//                salir = true;
-//                break;
-//
-//        }
-//        if (!salir && caso == 1) {
-//            switch (segundoCaso) {
-//                case 1:
-//                    g->vaciar();
-//                    break;
-//                case 2:
-//                    if(g->vacio())
-//                        cout << "El grafo esta vacio" << "\n";
-//                    else
-//                        cout << "El grafo no esta vacio"<< "\n";
-//                    break;
-//                case 3:
-//                    cout << "Escriba la etiqueta del vertice: ";
-//                    cin >> etiqueta;
-//                    g->agregarVertice(etiqueta);
-//                    break;
-//                case 4:
-//                    cout << "Escriba la etiqueta del vertice: ";
-//                    cin >> etiqueta;
-//                    v = algoritmos->buscarEtiq(etiqueta,*g);
-//                    g->eliminarVertice(v);
-//                    break;
-//                case 5:
-//                    cout << "Escriba la etiqueta del vertice: ";
-//                    cin >> etiqueta;
-//                    v = algoritmos->buscarEtiq(etiqueta,*g);
-//                    cout << "Escriba la nueva etiqueta del vertice: ";
-//                    cin >> etiqueta;
-//                    g->modificarEtiqueta(v,etiqueta);
-//                    break;
-//                case 6:
-//                    cout << "Escriba la etiqueta del vertice: ";
-//                    cin >> etiqueta;
-//                    v = algoritmos->buscarEtiq(etiqueta,*g);
-//                    cout << g->Etiqueta(v);
-//                    break;
-//            }
-//            cout << "Presione la tecla 1 para seguir con operadores basicos." << "\n";
-//            cout << "Presione la tecla 2 para usar algoritmos." << "\n";
-//            cout << "Presione la tecla 3 para salir." << "\n";
-//            cin >> caso;
-//        } else if (!salir && caso == 2) {
-//            switch (segundoCaso) {
-//
-//            }
-//            cout << "Presione la tecla 1 para seguir con operadores basicos." << "\n";
-//            cout << "Presione la tecla 2 para usar algoritmos." << "\n";
-//            cout << "Presione la tecla 3 para salir." << "\n";
-//            cin >> caso;
-//        }
-//        if(caso == 3){
-//            salir = true;
-//            cout << "adios";
-//        }
-//        
-//    }
-//   
-//
+    grafo* g = new grafo;
+    vertice v;
+    string etiqueta;
+    AlgoritmosGrafos* algoritmos = new AlgoritmosGrafos();
+    int caso = 0;
+    cout << "Saludos, ¿que desea utilizar?" << "\n";
+    cout << "Presione la tecla 1 para operadores basicos." << "\n";
+    cout << "Presione la tecla 2 para algoritmos." << "\n";
+    cout << "Presione la tecla 3 para salir." << "\n";
+    bool salir = false;
+    while (!salir) {
+        cin >> caso;
+        int segundoCaso = 0;
+        switch (caso) {
+            case 1:
+                cout << "Presione 1 para vaciar()" << "\n";
+                cout << "Presione 2 para vacio()" << "\n";
+                cout << "Presione 3 para agregarVertice()" << "\n";
+                cout << "Presione 4 para eliminarVertice()" << "\n";
+                cout << "Presione 5 para modifiarEtiqueta()" << "\n";
+                cout << "Presione 6 para Etiqueta()" << "\n";
+                cout << "Presione 7 para agregarArista()" << "\n";
+                cout << "Presione 8 para eliminarArista()" << "\n";
+                cout << "Presione 9 para modificarPeso()" << "\n";
+                cout << "Presione 10 para peso()" << "\n";
+                cout << "Presione 11 para adyacentes()" << "\n";
+                cout << "Presione 12 para primerVertice()" << "\n";
+                cout << "Presione 13 para siguienteVertice()" << "\n";
+                cout << "Presione 14 para primerVerticeAdy()" << "\n";
+                cout << "Presione 15 para sigVerticeAdy()" << "\n";
+                cout << "Presione 16 para numVertices()" << "\n";
+                cout << "Presione 17 para numVerticesAdy()" << "\n";
+                cin >> segundoCaso;
+                break;
+
+            case 2:
+                cout << "Presione 1 para Dijkstra()" << "\n";
+                cout << "Presione 2 para Floyd()" << "\n";
+                cout << "Presione 3 para Prim()" << "\n";
+                cout << "Presione 4 para Kruskal()" << "\n";
+                cout << "Presione 5 para Copiar()" << "\n";
+                cout << "Presione 6 para Iguales()" << "\n";
+                cout << "Presione 7 para Vendedor()"<< "\n";
+                cin >> segundoCaso;
+                break;
+            case 3:
+                cout << "Adios" << "\n";
+                salir = true;
+                break;
+
+        }
+        if (!salir && caso == 1) {
+            switch (segundoCaso) {
+                case 1:
+                    g->vaciar();
+                    break;
+                case 2:
+                    if(g->vacio())
+                        cout << "El grafo esta vacio" << "\n";
+                    else
+                        cout << "El grafo no esta vacio"<< "\n";
+                    break;
+                case 3:
+                    cout << "Escriba la etiqueta del vertice: ";
+                    cin >> etiqueta;
+                    g->agregarVertice(etiqueta);
+                    break;
+                case 4:
+                    cout << "Escriba la etiqueta del vertice: ";
+                    cin >> etiqueta;
+                    v = algoritmos->buscarEtiq(etiqueta,*g);
+                    g->eliminarVertice(v);
+                    break;
+                case 5:
+                    cout << "Escriba la etiqueta del vertice: ";
+                    cin >> etiqueta;
+                    v = algoritmos->buscarEtiq(etiqueta,*g);
+                    cout << "Escriba la nueva etiqueta del vertice: ";
+                    cin >> etiqueta;
+                    g->modificarEtiqueta(v,etiqueta);
+                    break;
+                case 6:
+                    cout << "Escriba la etiqueta del vertice: ";
+                    cin >> etiqueta;
+                    v = algoritmos->buscarEtiq(etiqueta,*g);
+                    cout << g->Etiqueta(v);
+                    break;
+            }
+            cout << "Presione la tecla 1 para seguir con operadores basicos." << "\n";
+            cout << "Presione la tecla 2 para usar algoritmos." << "\n";
+            cout << "Presione la tecla 3 para salir." << "\n";
+            cin >> caso;
+        } else if (!salir && caso == 2) {
+            switch (segundoCaso) {
+
+            }
+            cout << "Presione la tecla 1 para seguir con operadores basicos." << "\n";
+            cout << "Presione la tecla 2 para usar algoritmos." << "\n";
+            cout << "Presione la tecla 3 para salir." << "\n";
+            cin >> caso;
+        }
+        if(caso == 3){
+            salir = true;
+            cout << "adios";
+        }
+        
+    }
+   
+
 ////
 
      return 0;
